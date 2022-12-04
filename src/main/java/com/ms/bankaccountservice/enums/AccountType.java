f47640519d6371ab967118b2659cfc21955ce049
NassimMs <49593722+NassimMs@users.noreply.github.com>
2022-12-04 21:09:43 +0000

    Delete src directory

--- src/main/java/com/ms/bankaccountservice/enums/AccountType.java
+++
@@ -1,5 +0,0 @@
-package com.ms.bankaccountservice.enums;
-
-public enum AccountType {
-    CURRENT_ACCOUNT,SAVING_ACCOUNT
-}
--- src/main/java/com/ms/bankaccountservice/repositories/BankAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ms.bankaccountservice.repositories;
-
-import com.ms.bankaccountservice.entities.BankAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
-}
